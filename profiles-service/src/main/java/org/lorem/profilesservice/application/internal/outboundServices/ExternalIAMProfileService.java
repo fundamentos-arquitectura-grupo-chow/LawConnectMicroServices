5aae20a562d5100ed902412aa1cc5c3364bac32c
Fabricio <115115308+Fabricio011001@users.noreply.github.com>
2025-06-14 17:47:09 -0500

    fix(feature/profiles-service): Added BC of Profiles Service

--- profiles-service/src/main/java/org/lorem/profilesservice/application/internal/outboundServices/ExternalIAMProfileService.java
+++ profiles-service/src/main/java/org/lorem/profilesservice/application/internal/outboundServices/ExternalIAMProfileService.java
@@ -1,24 +1,24 @@
-/*
+
 package org.lorem.profilesservice.application.internal.outboundServices;
 
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-import upc.LoremIpsum.lawconnectplatform.iam.interfaces.acl.IamContextFacade;
+// import upc.LoremIpsum.lawconnectplatform.iam.interfaces.acl.IamContextFacade;
 
 @Service
 public class ExternalIAMProfileService {
 
-    private final IamContextFacade iamContextFacade;
+   // private final IamContextFacade iamContextFacade;
 
     public ExternalIAMProfileService(
-            @Lazy IamContextFacade iamContextFacade
+           // @Lazy IamContextFacade iamContextFacade
     ) {
-        this.iamContextFacade = iamContextFacade;
+        // this.iamContextFacade = iamContextFacade;
     }
 
     public Long getUserIdByUsername(String username) {
-        return iamContextFacade.fetchUserIdByUsername(username);
+        // return iamContextFacade.fetchUserIdByUsername(username);
     }
 
 }
-*/
+
